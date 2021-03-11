7accb5e19bce87529e8a93c1a02bd237a4d30bdf
Vincentdm31 <vincent.dimarco1@gmail.com>
2021-03-11 09:49:42 +0100

    piece delete

--- resources/views/pieces/show.blade.php
+++ resources/views/pieces/show.blade.php
@@ -2,33 +2,34 @@
 
 @section('content')
 <div class="container mt-5">
-    <div class="container d-flex h100 mt-5">
-        <div class="container card my-auto mx-auto shadow-2">
-            <div class="card-header dark txt-white">
-                <div class="grix xs1 md2">
+    <div class="container">
+        <div class="card grey light-4 my-auto mx-auto shadow-2 rounded-2">
+            <div class="grix xs1 md2">
+                <div class="p-4">
                     <div>
-                        <p id="name">Nom: {{ $pieceList->name }}</p>
-                        <p id="ref">Référence: {{ $pieceList->ref }}</p>
+                        <p class="m-0 txt-airforce txt-dark-4 font-s3">Nom</p>
+                        <em class="m-0 txt-airforce txt-dark-4 font-s2" id="name">{{ $pieceList->name }}</em>
                     </div>
-                    <div class="mx-auto">
-                        <p>Prix : {{ $pieceList->price }} €/u</p>
-                        <p>Stock disponible : {{ $pieceList->qte }}</p>
+                    <div class="mt-3">
+                        <p class="m-0 txt-airforce txt-dark-4 font-s3">Référence</p>
+                        <em class="m-0 txt-airforce txt-dark-4 font-s2" id="ref">{{ $pieceList->ref }}</em>
+                    </div>
+                    <div class="mt-3">
+                        <p class="m-0 txt-airforce txt-dark-4 font-s3">Stock</p>
+                        <em class="m-0 txt-airforce txt-dark-4 font-s2" id="ref">{{ $pieceList->qte }}</em>
+                    </div>
+                    <div class="mt-3">
+                        <p class="m-0 txt-airforce txt-dark-4 font-s3">Prix</p>
+                        <em class="m-0 txt-airforce txt-dark-4 font-s2" id="ref">{{ $pieceList->price }} €/u</em>
                     </div>
                 </div>
-
-
+                <div class="d-flex fx-col">
+                    <img src="{{ asset('storage/images/'.$pieceList->path) }}" id="imgPiece" class="responsive-media p-3 mx-auto">
+                    <button id="btn" class="btn orange dark-1 txt-white mx-auto dark-2 mt-2 mb-2 rounded-2" onclick="PrintElem(btn)">Imprimer</button>
+                </div>
             </div>
-            <div class="card-content d-flex fx-col">
-                <img src="{{ asset('storage/images/'.$pieceList->path) }}" id="imgPiece" class="responsive-media mx-auto p-3">
-                <button id="btn" class="btn orange dark-1 txt-white large mx-auto dark-2 mt-5" onclick="PrintElem(btn)">Imprimer</button>
-            </div>
-
         </div>
     </div>
-
-</div>
-</div>
-
 </div>
 
 @endsection
@@ -41,7 +42,6 @@
         let ref = document.getElementById('ref').innerHTML;
 
         var mywindow = window.open('', 'PRINT', 'height=400,width=600');
-
         mywindow.document.write('<html><head><title>' + document.title + '</title>');
         mywindow.document.write('</head><body >');
         mywindow.document.write('<h1>' + name + '</h1>');
